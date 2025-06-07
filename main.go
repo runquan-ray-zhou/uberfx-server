cb01d40b7c20a05f973229a700002202b435d1a5
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-06-07 00:36:48 -0400

    Add NewHandler and NewServerMux

--- main.go
+++ main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"os"
 
 	"go.uber.org/fx"
 )
 
 func main() {
 	fx.New(
-		fx.Provide(NewHTTPServer),
+		fx.Provide(
+			NewHTTPServer,
+			NewServeMux,
+			NewHandler,
+		),
+
 		fx.Invoke(func(*http.Server) {}),
 	).Run()
 }
 
-func NewHTTPServer(lc fx.Lifecycle) *http.Server {
-	srv := &http.Server{Addr: ":8080"}
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			ln, err := net.Listen("tcp", srv.Addr)
@@ -33,4 +40,22 @@ func NewHTTPServer(lc fx.Lifecycle) *http.Server {
 		},
 	})
 	return srv
+}
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+	}
+}
+
+func NewServeMux(h *Handler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/", h)
+	return mux
 }

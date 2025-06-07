46e3b012882ddd69a1e653007fdcc8c53f4ef40c
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-06-07 01:00:51 -0400

    Add zap logger

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 go mod init github.com/runquan-ray-zhou/uberfx-server
 go mod vendor
 go mod tidy
+
+Start Server
 go run main.go
--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"net"
 	"net/http"
-	"os"
 
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
 )
 
 func main() {
 	fx.New(
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
 		fx.Provide(
 			NewHTTPServer,
 			NewServeMux,
 			NewHandler,
+			zap.NewExample,
 		),
 
 		fx.Invoke(func(*http.Server) {}),
 	).Run()
 }
 
-func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
 	srv := &http.Server{Addr: ":8080", Handler: mux}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
@@ -31,7 +35,7 @@ func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
 			if err != nil {
 				return err
 			}
-			fmt.Println("Starting HTTP server at", srv.Addr)
+			log.Info("Starting HTTP server", zap.String("addr", srv.Addr))
 			go srv.Serve(ln)
 			return nil
 		},
@@ -42,15 +46,19 @@ func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
 	return srv
 }
 
-type Handler struct{}
-
-func NewHandler() *Handler {
-	return &Handler{}
+type Handler struct {
+	log *zap.Logger
 }
 
-func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func NewHandler(log *zap.Logger) *Handler {
+	return &Handler{
+		log: log,
+	}
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if _, err := io.Copy(w, r.Body); err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+		h.log.Warn("Failed to handle request", zap.Error(err))
 	}
 }
 

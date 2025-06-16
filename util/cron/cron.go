5e486691a27d7b083e5eab1385cbec8f71f6116a
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-06-16 02:13:45 -0400

    upate cron job url to be local server backend

--- util/cron/cron.go
+++ util/cron/cron.go
@@ -14,7 +14,7 @@ func NewCronJob() {
 
 	// Schedule the job to run every 15 minutes
 	c.AddFunc("@every 15m", func() {
-		resp, err := http.Get("https://your-backend.com/health") // or your own endpoint
+		resp, err := http.Get("http://localhost:8080/") // rest handler endpoint
 		if err != nil {
 			fmt.Println("Ping failed:", err)
 			return
